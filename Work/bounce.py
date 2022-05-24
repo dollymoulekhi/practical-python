9f16d6bf2c7a5c48eabcb6f86ef8103bda8fcfb5
Dolly Moulekhi <dollymoulekhi@BPSSMBA151s-MacBook-Air.local>
2022-05-24 09:45:25 +0530

    1st module code

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,16 @@
 # bounce.py
 #
 # Exercise 1.5
+height=100 # in meters
+
+# new_height=3/5* height # should be insie loop
+
+# have to show the height of 1st 10 bounces
+
+no_of_bounces=1
+
+while no_of_bounces<=10:
+    print('no_of_bounces',no_of_bounces)
+    print('height of bounce',height)
+    height=round(3* (height)/5.0,2)
+    no_of_bounces=no_of_bounces+1
